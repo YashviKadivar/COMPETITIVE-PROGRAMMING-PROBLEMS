982c728d9dc3dcb88054da16d5c1cc667529ae43
YashviKadivar <112105269+YashviKadivar@users.noreply.github.com>
2022-09-30 10:32:30 +0530

    Add files via upload

---
+++ problem 2.cpp
@@ -0,0 +1,33 @@
+/* Product of Array Except Self */
+
+#include<iostream>
+using namespace std;
+
+int pickIndexOfMountainArray(int *arr,int size)
+{
+    int index=-1;
+    int max=-123;
+
+    for(int i=0;i<size;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+            index=i;
+        }
+    }
+
+    cout<<"element at pick -> "<<max<<endl;
+
+    return index;
+}
+
+int main()
+{
+    int arr[5]={1,2,3,4};
+    int size=5;
+
+    cout<<pickIndexOfMountainArray(arr,size);
+
+    return 0;
+}
